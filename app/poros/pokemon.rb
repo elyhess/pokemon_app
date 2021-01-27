bef128f238648e8170c1f2de662deba5f1858070
Ely Hess <Ely.hess@me.com>
2021-01-27 10:37:00 -0700

    Reformat

--- app/poros/pokemon.rb
+++ app/poros/pokemon.rb
@@ -1,9 +1,11 @@
 class Pokemon
-	attr_reader :name, :id, :picture
+	attr_reader :name,
+	            :id,
+	            :picture
 
 	def initialize(data)
-		@name = data[:name]
-		@id = data[:id]
+		@name    = data[:name]
+		@id      = data[:id]
 		@picture = data[:sprites][:front_default]
 	end
 
