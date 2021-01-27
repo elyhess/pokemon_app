8c5c53cced8ef55bcb804980ed4b3786ddc3adde
Ely Hess <Ely.hess@me.com>
2021-01-27 10:46:48 -0700

    Refactor service

--- app/facades/poke_facade.rb
+++ app/facades/poke_facade.rb
@@ -1,9 +1,7 @@
 class PokeFacade
 	class << self
 		def get_a_pokemon(pokemon)
-			conn = Faraday.new("https://pokeapi.co")
-			response = conn.get("/api/v2/pokemon-form/#{pokemon}/")
-			data = JSON.parse(response.body, symbolize_names: true)
+			data = PokeService.call_for_a_pokemon(pokemon)
 			Pokemon.new(data)
 		end
 	end
