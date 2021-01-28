3f57b95338a44799bc6153a32000ec2ac3ac29f0
Ely Hess <Ely.hess@me.com>
2021-01-27 18:26:33 -0700

    Add stub_get_json helper

--- spec/features/user_can_search_for_pokemon_spec.rb
+++ spec/features/user_can_search_for_pokemon_spec.rb
@@ -3,17 +3,21 @@ require 'rails_helper'
 RSpec.describe "as a visitor" do
   describe "when I search for a pokemon" do
     it 'I see results' do
-      save_and_open_page
+      filename = "pokemon.json"
+      url      = "https://pokeapi.co/api/v2/pokemon-form/mewtwo/"
+
+	    stub_get_json(url, filename)
+
       visit root_path
 
-      fill_in :pokemon, with: "Pikachu"
+      fill_in :pokemon, with: "mewtwo"
 
       click_button "Search"
 
       expect(current_path).to eq(pokemon_index_path)
 
-      expect(page).to have_content("Pikachu")
-      expect(page).to have_content("Id #25")
+      expect(page).to have_content("Mewtwo")
+      expect(page).to have_content("Id #150")
     end
   end
 end
